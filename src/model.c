807849c71d9cbf04f91a37373b49b0dd81339054
Lawrence Murray <lawrence@indii.org>
2022-11-12 22:15:27 -0800

    Debugging model forward and backward passes.

--- src/model.c
+++ src/model.c
@@ -20,8 +20,8 @@ void model_init(model_t* m, const int M, const int B, const int L,
   /* allocate */
   cudaMalloc((void**)&m->theta, P*sizeof(float));
   cudaMalloc((void**)&m->dtheta, P*sizeof(float));
-  cudaMalloc((void**)&m->Z, U*sizeof(float));
-  cudaMalloc((void**)&m->dZ, U*sizeof(float));
+  cudaMalloc((void**)&m->Z, U*B*sizeof(float));
+  cudaMalloc((void**)&m->dZ, U*B*sizeof(float));
   cudaMalloc((void**)&m->l, B*sizeof(float)); 
   cudaMallocHost((void**)&m->ll, sizeof(float));
 
@@ -62,25 +62,24 @@ void model_term(model_t* m) {
 void model_forward(model_t* m, float* X, const int B) {
   assert(B <= m->B);
 
+  int U = m->U;
   int M = m->M;
   int L = m->L;
-  float* y = X + M - 1;  // last field is label
-
-  int u = m->u[0];
   int u_prev = M - 1;
+  int u = m->u[0];
 
   /* first layer weights, biases and activations */
   float* W = m->theta;
   float* b = W + u*u_prev;
   float* Z = m->Z;
 
-  cublasSetMatrix(u, B, sizeof(float), b, 0, Z, u);
+  cublasSetMatrix(u, B, sizeof(float), b, 0, Z, U);
   cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, u, B, u_prev, scalar1, W, u,
       X, M, scalar1, Z, u);
 
   for (int l = 1; l < L; ++l) {
-    u = m->u[l];
     u_prev = m->u[l - 1];
+    u = m->u[l];
 
     /* rectify input layer */
     float* Z_prev = Z;
@@ -89,13 +88,13 @@ void model_forward(model_t* m, float* X, const int B) {
     /* next layer weights, biases and activations */
     W = b + u_prev;
     b = W + u*u_prev;
-    Z = Z + u_prev;
+    Z = Z + B*u_prev;
 
     cublasSetMatrix(u, B, sizeof(float), b, 0, Z, u);
     cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, u, B, u_prev, scalar1,
         W, u, Z_prev, u_prev, scalar1, Z, u);
   }
-  log_likelihood(B, y, M, Z, u, m->l, 1);
+  log_likelihood(B, X + M - 1, M, Z, u, m->l, 1);
   cublasSdot(handle, B, m->l, 1, scalar1, 0, m->ll);
 }
 
@@ -106,35 +105,35 @@ void model_backward(model_t* m, float* X, const int B) {
   int P = m->P;
   int M = m->M;
   int L = m->L;
-  float* y = X + M - 1;  // last field is label
 
   int u = m->u[L - 1];
   int u_prev = L >= 2 ? m->u[L - 2] : M - 1;
 
   float* W = m->theta + P;
   float* b = NULL;
-  float* Z = m->Z + U - u;
-  float* Z_prev = NULL;
+  float* Z = NULL;
+  float* Z_prev = m->Z + B*U - B*u;
 
   float* dW = m->dtheta + P;
   float* db = NULL;
-  float* dZ = m->dZ + U - u;
-  float* dZ_prev = NULL;
+  float* dZ = NULL;
+  float* dZ_prev = m->dZ + B*(U - u);
 
-  log_likelihood_grad(B, y, M, Z, u, dZ, u);
+  log_likelihood_grad(B, X + M - 1, M, Z_prev, u, dZ_prev, u);
   for (int l = L - 1; l > 0; --l) {
-    u = m->u[l];
     u_prev = m->u[l - 1];
+    u = m->u[l];
+
+    Z = Z_prev;
+    dZ = dZ_prev;
 
     b = W - u;
     W = b - u*u_prev;
-    Z = Z - u;
-    Z_prev = Z - u_prev;
+    Z_prev = Z - B*u_prev;
 
     db = dW - u;
     dW = db - u*u_prev;
-    dZ = dZ - u;
-    dZ_prev = dZ - u_prev;
+    dZ_prev = dZ - B*u_prev;
 
     cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, u_prev, B, u, scalar1, W, u,
         dZ, u, scalar0, dZ_prev, u_prev);
@@ -145,23 +144,24 @@ void model_backward(model_t* m, float* X, const int B) {
         scalar0, db, 1);
   }
 
-  u = m->u[0];
   u_prev = M - 1;
+  u = m->u[0];
 
-  b = W - u;
-  W = W - u*u_prev;
-  Z = Z - u;
-  assert(W == m->theta);
+  Z = Z_prev;
   assert(Z == m->Z);
-
-  db = dW - u;
-  dW = dW - u*u_prev;
-  dZ = dZ - u;
-  assert(dW == m->dtheta);
+  dZ = dZ_prev;
   assert(dZ == m->dZ);
 
+  b = W - u;
+  W = b - u*u_prev;
+  assert(W == m->theta);
+
+  db = dW - u;
+  dW = db - u*u_prev;
+  assert(dW == m->dtheta);
+
   cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, u, u_prev, B, scalar1, dZ, u,
-      X, u_prev, scalar0, dW, u);
+      X, M, scalar0, dW, u);
   cublasSgemv(handle, CUBLAS_OP_N, u, B, scalar1, dZ, u, scalar1, 0, scalar0,
       db, 1);
 }
