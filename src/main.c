3e86f413ff81b31d20eef2d24522fc355a370418
Lawrence Murray <lawrence@indii.org>
2023-01-06 02:02:53 +0000

    Implemented shuffle with event.

--- src/main.c
+++ src/main.c
@@ -27,43 +27,46 @@ int main(const int argc, const char *argv[]) {
   optimizer_init(&o, m.P, 1.0e-3f, 0.9f, 0.999f, 1.0e-8f);
 
   /* train */
-  for (int epoch = 1; epoch <= 200; ++epoch) {
-    printf("epoch %d: ", epoch);
+  float* X_train = NULL;
+  size_t bytes = d.M*d.N_train*sizeof(float);
+  cudaMalloc((void**)&X_train, bytes);
+  cudaEvent_t event;
+  cudaEventCreate(&event);
+
+  for (int epoch = 1; epoch <= 100; ++epoch) {
+    /* copy up shuffled data */
+    cudaMemcpyAsync(X_train, d.X_train, bytes, cudaMemcpyDefault,
+        cudaStreamDefault);
+    cudaEventRecord(event, cudaStreamDefault);
 
     /* train */
     for (int i = 0; i < d.N_train; i += B) {
       int b = (i + B <= d.N_train) ? B : d.N_train - i;
-      model_forward(&m, d.X_train + i*d.M, b);
-      model_backward(&m, d.X_train + i*d.M, b);
+      model_forward(&m, X_train + i*d.M, b);
+      model_backward(&m, X_train + i*d.M, b);
       optimizer_step(&o, m.theta, m.dtheta);
     }
-    cudaDeviceSynchronize();
-
-    /* training loss */
-    *m.ll = 0.0f;
-    for (int i = 0; i < d.N_train; i += B) {
-      int b = (i + B <= d.N_train) ? B : d.N_train - i;
-      model_forward(&m, d.X_train + i*d.M, b);
-      model_predict(&m, d.X_train + i*d.M, b);
-    }
-    cudaDeviceSynchronize();
-    printf("train loss %f ", -*m.ll/d.N_train);
 
     /* test loss */
-    *m.ll = 0.0f;
     for (int i = 0; i < d.N_test; i += B) {
       int b = (i + B <= d.N_test) ? B : d.N_test - i;
       model_forward(&m, d.X_test + i*d.M, b);
       model_predict(&m, d.X_test + i*d.M, b);
     }
-    cudaDeviceSynchronize();
-    printf("test loss %f\n", -*m.ll/d.N_test);
 
     /* shuffle data for next time */
+    cudaEventSynchronize(event);
     shuffle(d.M, d.N_train, d.X_train, d.M);
+
+    /* report */
+    cudaDeviceSynchronize();
+    fprintf(stderr, "epcoh %d: test loss %f\n", epoch, -*m.ll/d.N_test);
+    *m.ll = 0.0f;
   }
 
   /* clean up */
+  cudaFree(X_train);
+  cudaEventDestroy(event);
   optimizer_term(&o);
   model_term(&m);
   data_term(&d);
