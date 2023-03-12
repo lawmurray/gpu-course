3f1514ae870976698ec5d1eb0d6e62efbaafad85
Lawrence Murray <lawrence@indii.org>
2023-03-12 17:51:07 +0000

    Tested and fixed convolution kernel implementations.

--- src/convolve.cu
+++ src/convolve.cu
@@ -11,69 +11,71 @@ __global__ void kernel_convolve_v0(const int m, const int n, const float* p,
     const int incr) {
   /* element of r for which thread is responsible */
   int i = threadIdx.y + blockIdx.y*blockDim.y;
-  
-  /* sum across elements in each thread */
-  float result = 0.0f;
-  for (int j = 0; j < n; ++j) {
-    if (0 <= i - j && i - j < m) {
-      result += p[(i - j)*incp]*q[j*incq];
+
+  if (i < m + n - 1) {
+    float result = 0.0f;
+    for (int j = 0; j < n; ++j) {
+      if (0 <= i - j && i - j < m) {
+        result += p[(i - j)*incp]*q[j*incq];
+      }
     }
+    r[i] = result;
   }
-  
-  /* write element */
-  r[i] = result;
 }
 
 __global__ void kernel_convolve_v1(const int m, const int n, const float* p,
     const int incp, const float* q, const int incq, float* r,
     const int incr) {
-  /* first element of r for which thread is responsible */
+  /* element of r for which thread is responsible */
   int i = threadIdx.y + blockIdx.y*blockDim.y;
   
-  /* sum across elements in each thread */
-  float result1 = 0.0f, result2 = 0.0f;
-  for (int j = 0; j < n; ++j) {
-    if (0 <= i - j) {
-      result1 += p[(i - j)*incp]*q[j*incq];
-    } else {
-      result2 += p[(m + i - j)*incp]*q[j*incq];
+  if (i < m) {
+    float result1 = 0.0f, result2 = 0.0f;
+    for (int j = 0; j < n; ++j) {
+      if (0 <= i - j) {
+        result1 += p[(i - j)*incp]*q[j*incq];
+      } else {
+        result2 += p[(m + i - j)*incp]*q[j*incq];
+      }
     }
-  }
-  
-  /* write elements */
-  r[i] = result1;
-  if (i < m - 1) {
-    r[i + m] = result2;
+    r[i] = result1;
+    if (i < m - 1) {
+      r[i + n] = result2;
+    }
   }
 }
 
 __global__ void kernel_convolve_v2(const int m, const int n, const float* p,
     const int incp, const float* q, const int incq, float* r,
     const int incr) {
-  __shared__ float* q_shared;
+  /* shared memory */
+  extern __shared__ float q_shared[];
+
+  /* element of r for which thread is responsible */
   int i = threadIdx.y + blockIdx.y*blockDim.y;
+
   float result1 = 0.0f, result2 = 0.0f;
-  
   for (int base = 0; base < n; base += blockDim.y) {
-    /* read the next batch into shared memory */
     int j = threadIdx.y;
-    q_shared[j] = base + j < n ? q[(base + j)*incq] : 0.0f;
+    __syncthreads();
+    q_shared[j] = (base + j < n) ? q[(base + j)*incq] : 0.0f;
     __syncthreads();
 
-    /* sum across elements in each thread  */
-    for (j = 0; j < blockDim.y; ++j) {
-      if (0 <= i - j) {
-        result1 += p[(base + i - j)*incp]*q_shared[j];
-      } else {
-        result2 += p[(base + m + i - j)*incp]*q_shared[j];
+    if (i < m) {
+      for (j = 0; j < blockDim.y; ++j) {
+        if (0 <= i - base - j) {
+          result1 += p[(i - base - j)*incp]*q_shared[j];
+        } else {
+          result2 += p[(m + i - base - j)*incp]*q_shared[j];
+        }
       }
     }
   }
-
-  /* set the final result */
-  r[i] = result1;
-  if (i < m - 1) {
-    r[i + m] = result2;
+  if (i < m) {
+    r[i] = result1;
+    if (i < m - 1) {
+      r[i + n] = result2;
+    }
   }
 }
 
@@ -82,23 +84,26 @@ __global__ void kernel_convolve_v3(const int m, const int n, const float* p,
     const int incr) {
   assert(blockDim.x == warpSize && gridDim.x == 1);
 
-  __shared__ float* q_shared;
-  int i = blockIdx.x*blockDim.x;
+  /* shared memory */
+  extern __shared__ float q_shared[];
+
+  /* element of r for which warp is responsible */
+  int i = threadIdx.y + blockIdx.y*blockDim.y;
+
   float result1 = 0.0f, result2 = 0.0f;
-  
   for (int base = 0; base < n; base += warpSize*blockDim.y) {
-    /* read the next batch into shared memory */
     int j = threadIdx.y*warpSize + threadIdx.x;
-    q_shared[j] = base + j < n ? q[(base + j)*incq] : 0.0f;
+    __syncthreads();
+    q_shared[j] = (base + j < n) ? q[(base + j)*incq] : 0.0f;
     __syncthreads();
 
-    /* sum across elements in each thread  */
-    for (j = threadIdx.x; j < warpSize*blockDim.y; j += warpSize) {
-      /* read the next batch into shared memory */
-      if (0 <= i - j) {
-        result1 += p[(base + i - j)*incp]*q_shared[j];
-      } else {
-        result2 += p[(base + m + i - j)*incp]*q_shared[j];
+    if (i < m) {
+      for (j = threadIdx.x; j < warpSize*blockDim.y; j += warpSize) {
+        if (0 <= i - base - j) {
+          result1 += p[(i - base - j)*incp]*q_shared[j];
+        } else {
+          result2 += p[(m + i - base - j)*incp]*q_shared[j];
+        }
       }
     }
   }
@@ -110,10 +115,10 @@ __global__ void kernel_convolve_v3(const int m, const int n, const float* p,
   }
 
   /* set the final result, only first thread in each warp */
-  if (threadIdx.x == 0) {
+  if (i < m && threadIdx.x == 0) {
     r[i] = result1;
     if (i < m - 1) {
-      r[i + m] = result2;
+      r[i + n] = result2;
     }
   }
 }
@@ -142,9 +147,9 @@ void convolve_v2(const int m, const int n, const float* p, const int incp,
 
 void convolve_v3(const int m, const int n, const float* p, const int incp,
     const float* q, const int incq, float* r, const int incr) {
-  dim3 block(32, BLOCK_SIZE);
+  dim3 block(32, BLOCK_SIZE/32);
   dim3 grid(1, (m + block.y - 1)/block.y);
-  size_t shared = block.y*sizeof(float);
+  size_t shared = block.x*block.y*sizeof(float);
   kernel_convolve_v3<<<grid,block,shared>>>(m, n, p, incp, q, incq, r, incr);
 }
 
--- src/discrete.c
+++ src/discrete.c
@@ -10,76 +10,98 @@ int main(const int argc, const char *argv[]) {
   /* initialize */
   int device = cuda_init(1);
 
-  const int m = 65536;
-  const int n = 65536;
-  float* p;
-  float* q;
-  float* r;
+  const int m = 65536, n = 65536;
+  float *p, *q, *r_v0, *r_v1, *r_v2, *r_v3;
   
   cudaMallocManaged((void**)&p, m*sizeof(float), cudaMemAttachGlobal);
   cudaMallocManaged((void**)&q, n*sizeof(float), cudaMemAttachGlobal);
-  cudaMallocManaged((void**)&r, (m + n - 1)*sizeof(float), cudaMemAttachGlobal);
+  cudaMallocManaged((void**)&r_v0, (m + n - 1)*sizeof(float), cudaMemAttachGlobal);
+  cudaMallocManaged((void**)&r_v1, (m + n - 1)*sizeof(float), cudaMemAttachGlobal);
+  cudaMallocManaged((void**)&r_v2, (m + n - 1)*sizeof(float), cudaMemAttachGlobal);
+  cudaMallocManaged((void**)&r_v3, (m + n - 1)*sizeof(float), cudaMemAttachGlobal);
 
-  float sum = 0.0f;
+  float sum_p = 0.0f;
   for (int i = 0; i < m; ++i) {
     p[i] = drand48();
-    sum += p[i];
+    sum_p += p[i];
   }
   for (int i = 0; i < m; ++i) {
-    p[i] /= sum;
+    p[i] /= sum_p;
   }
+
+  float sum_q = 0.0f;
   for (int i = 0; i < n; ++i) {
     q[i] = drand48();
-    sum += q[i];
+    sum_q += q[i];
   }
   for (int i = 0; i < n; ++i) {
-    q[i] /= sum;
+    q[i] /= sum_q;
   }
 
   cudaMemPrefetchAsync(p, m*sizeof(float), device, cudaStreamDefault);
   cudaMemPrefetchAsync(q, n*sizeof(float), device, cudaStreamDefault);
-  cudaMemPrefetchAsync(r, (m + n - 1)*sizeof(float), device, cudaStreamDefault);
+  cudaMemPrefetchAsync(r_v0, (m + n - 1)*sizeof(float), device, cudaStreamDefault);
+  cudaMemPrefetchAsync(r_v1, (m + n - 1)*sizeof(float), device, cudaStreamDefault);
+  cudaMemPrefetchAsync(r_v2, (m + n - 1)*sizeof(float), device, cudaStreamDefault);
+  cudaMemPrefetchAsync(r_v3, (m + n - 1)*sizeof(float), device, cudaStreamDefault);
 
   struct timeval s, e;
 
   /* v0 */
   cudaDeviceSynchronize();
   gettimeofday(&s, NULL);
-  convolve_v0(m, n, p, 1, q, 1, r, 1);
+  convolve_v0(m, n, p, 1, q, 1, r_v0, 1);
   cudaDeviceSynchronize();
   gettimeofday(&e, NULL);
-  float elapsed_v0 = (e.tv_sec - s.tv_sec)*1.0e6 + (e.tv_usec - s.tv_usec);
+  int elapsed_v0 = (e.tv_sec - s.tv_sec)*1e6 + (e.tv_usec - s.tv_usec);
 
   /* v1 */
   cudaDeviceSynchronize();
   gettimeofday(&s, NULL);
-  convolve_v1(m, n, p, 1, q, 1, r, 1);
+  convolve_v1(m, n, p, 1, q, 1, r_v1, 1);
   cudaDeviceSynchronize();
   gettimeofday(&e, NULL);
-  float elapsed_v1 = (e.tv_sec - s.tv_sec)*1.0e6 + (e.tv_usec - s.tv_usec);
+  int elapsed_v1 = (e.tv_sec - s.tv_sec)*1e6 + (e.tv_usec - s.tv_usec);
 
   /* v2 */
   cudaDeviceSynchronize();
   gettimeofday(&s, NULL);
-  convolve_v2(m, n, p, 1, q, 1, r, 1);
+  convolve_v2(m, n, p, 1, q, 1, r_v2, 1);
   cudaDeviceSynchronize();
   gettimeofday(&e, NULL);
-  float elapsed_v2 = (e.tv_sec - s.tv_sec)*1.0e6 + (e.tv_usec - s.tv_usec);
+  int elapsed_v2 = (e.tv_sec - s.tv_sec)*1e6 + (e.tv_usec - s.tv_usec);
 
   /* v3 */
   cudaDeviceSynchronize();
   gettimeofday(&s, NULL);
-  convolve_v3(m, n, p, 1, q, 1, r, 1);
+  convolve_v3(m, n, p, 1, q, 1, r_v3, 1);
   cudaDeviceSynchronize();
   gettimeofday(&e, NULL);
-  float elapsed_v3 = (e.tv_sec - s.tv_sec)*1.0e6 + (e.tv_usec - s.tv_usec);
+  int elapsed_v3 = (e.tv_sec - s.tv_sec)*1e6 + (e.tv_usec - s.tv_usec);
 
-  fprintf(stderr, "v0 %0.4fus\n", elapsed_v0);
-  fprintf(stderr, "v1 %0.4fus\n", elapsed_v1);
-  fprintf(stderr, "v2 %0.4fus\n", elapsed_v2);
-  fprintf(stderr, "v3 %0.4fus\n", elapsed_v3);
+  /* sum all output vectors as diagnostic, each should sum to 1 */
+  cudaDeviceSynchronize();
+  double sum_v0 = 0.0, sum_v1 = 0.0, sum_v2 = 0.0, sum_v3 = 0.0;
+  for (int i = 0; i < m + n - 1; ++i) {
+    sum_v0 += r_v0[i];
+    sum_v1 += r_v1[i];
+    sum_v2 += r_v2[i];
+    sum_v3 += r_v3[i];
+  }
+
+  /* output results */
+  fprintf(stderr, "v0    %6dus    %0.4f\n", elapsed_v0, sum_v0);
+  fprintf(stderr, "v1    %6dus    %0.4f\n", elapsed_v1, sum_v1);
+  fprintf(stderr, "v2    %6dus    %0.4f\n", elapsed_v2, sum_v2);
+  fprintf(stderr, "v3    %6dus    %0.4f\n", elapsed_v3, sum_v3);
 
   /* clean up */
+  cudaFree(p);
+  cudaFree(q);
+  cudaFree(r_v0);
+  cudaFree(r_v1);
+  cudaFree(r_v2);
+  cudaFree(r_v3);
   cuda_term();
 
   return 0;
